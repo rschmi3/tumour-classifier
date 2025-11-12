fa64a0e326098be810b1bf6598a2cb7ae1cc9be5
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-11-12 07:46:34 +0000

    only build the flake for linux

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
